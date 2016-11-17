6b15c8956a174d264a117c04e686b156fd7e3592
pm352 <furfari.paul@gmail.com>
2016-11-17 12:59:39 +0100

    original project

---
+++ inc/settings.dist.php
@@ -0,0 +1,4 @@
+<?php
+
+// Ce fichier est la copie de settings.php, mais sans les valeurs
+// Ce fichier peut-être versionné
---
+++ inc/settings.php
@@ -0,0 +1,4 @@
+<?php
+
+// Mettre ici les constantes pour la connexion, mais aucun code
+// Ce fichier ne doit pas être versionné
