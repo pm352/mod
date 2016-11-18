5c1189fd3d7c6cb08accaec2717b1c5e257e8d0d
neby56 <johnny101091@hotmail.com>
2016-11-18 14:50:42 +0100

    un début de détail movies

---
+++ movie.php
@@ -0,0 +1,28 @@
+<?php
+
+include_once 'inc/config.php';
+
+$movId = isset($_GET['id'])? intval($_GET['id']) : 0;
+
+$sql = '
+SELECT mov_title, mov_file, cat_name, act_name, typ_name, mov_description, mov_release_date, mov_file 
+FROM movies 
+LEFT OUTER JOIN movies_has_actors ON movies_has_actors.movies_mov_id = movies.mov_description
+LEFT OUTER JOIN actors ON actors.act_id = movies_has_actors.actors_act_id
+LEFT OUTER JOIN category ON category.cat_id = movies.category_cat_id
+LEFT OUTER JOIN type_stockage ON type_stockage.typ_id = movies.type_stockage_typ_id
+WHERE mov_id = :movId';
+
+$sth = $pdo->prepare($sql);
+$sth->bindValue(':movId', $movId, PDO::PARAM_INT);
+
+if($sth->execute()){
+	$donnees = $sth->fetchAll(PDO::FETCH_ASSOC);
+}else{
+	print_r($pdo->errorInfo());
+}
+
+// VIEW
+include 'views/header.php';
+include 'views/movieView.php';
+include 'views/footer.php';
---
+++ views/movieView.php
@@ -0,0 +1,4 @@
+<h2><?= $donnees[0]['mov_title'] ?></h2>
+<p><?=$donnees[0]['mov_description'] ?></p>
+<p><?=$donnees[0]['act_name'] ?></p>
+<p><?=$donnees[0]['mov_file'] ?></p>
