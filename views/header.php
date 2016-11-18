6a1ac4d715f8ddd8ebe7182b1dcae6c356d2754a
pm352 <furfari.paul@gmail.com>
2016-11-18 10:11:14 +0100

    header

--- views/header.php
+++ views/header.php
@@ -6,38 +6,67 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 		<link rel="stylesheet" href="assets/css/style.css">
 	</head>
 
 	<body>
-		<div id="maxsize">
-			<div class="container">
-				<nav class="navbar navbar-pills">
-					<div class="container-fluid">
-						<!-- Brand and toggle get grouped for better mobile display -->
-						<div class="navbar-header">
-							<a class="navbar-brand" href="index.php"><span class="glyphicon glyphicon-home" aria-hidden="true"></span></a>
-						</div>
+		<nav class="navbar navbar-default" role="navigation">
+		  <div class="container-fluid">
+		    <!-- Brand and toggle get grouped for better mobile display -->
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		    </div>
 
-						<!-- Collect the nav links, forms, and other content for toggling -->
-						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-							<ul class="nav navbar-nav">
-								<li class="active"><a href="index.php">Acceuil</a></li>
-								<li><a href="add.php">Catégories</a></li>
-								<li><a href="list.php">Ajout d'un film</a></li>
-								<?php if (!empty($_SESSION['userId'])):?>
-								<li><a href="sessions_delete.php">Déconnexion</a></li>
-								<?php else :?>
-								<li><a href="signup.php">S'inscrire</a></li>
-								<li><a href="signin.php">Se connecter</a></li>
-								<?php endif;?>
-							</ul>
-							<form action="list.php" class="navbar-form navbar-right">
-								<div class="form-group">
-									<input type="text" name="q" value="<?= isset($search)? $search:'' ?>" class="form-control" placeholder="Recherche">
-								</div>
-								<button type="submit"  class="btn btn-default">Rechercher</button>
-							</form>
-						</div><!-- /.navbar-collapse -->
-					</div><!-- /.container-fluid -->
-				</nav>
+		    <!-- Collect the nav links, forms, and other content for toggling -->
+		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		      <ul class="nav navbar-nav">
+		        <li class="active"><a href="#">Acceuil</a></li>
+		        <li><a href="#">Catégories</a></li>
+		        <li><a href="#">Ajout d'un film</a></li>
+		      </ul>
+		      <form class="navbar-form navbar-left" role="search">
+		        <div class="form-group">
+		          <input type="text" class="form-control" placeholder="Search">
+		        </div>
+		        <button type="submit" class="btn btn-default">Submit</button>
+		      </form>
+		      <ul class="nav navbar-nav navbar-right">
+		        <li class="dropdown">
+		          <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Login</b> <span class="caret"></span></a>
+					<ul id="login-dp" class="dropdown-menu">
+						<li>
+							 <div class="row">
+									<div class="col-md-12">
+										Login
+										 <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
+												<div class="form-group">
+													 <label class="sr-only" for="exampleInputEmail2">Email address</label>
+													 <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" required>
+												</div>
+												<div class="form-group">
+													 <label class="sr-only" for="exampleInputPassword2">Password</label>
+													 <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required>
+		                                             <div class="help-block text-right"><a href="">Forget the password ?</a></div>
+												</div>
+												<div class="form-group">
+													 <button type="submit" class="btn btn-primary btn-block">Sign in</button>
+												</div>
+										 </form>
+									</div>
+									<div class="bottom text-center">
+										<a href="#"><b>S'inscrire</b></a>
+									</div>
+							 </div>
+						</li>
+					</ul>
+		        </li>
+		      </ul>
+		    </div><!-- /.navbar-collapse -->
+		  </div><!-- /.container-fluid -->
+		</nav>
