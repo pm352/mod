2cce0dc9832eff6e163d5961cbd53774147956a6
DiPaLu <seiya@windowslive.com>
2016-11-18 12:16:27 +0100

    création catalog

---
+++ catalog.php
@@ -0,0 +1,25 @@
+<?php
+require_once 'inc/config.php';
+
+
+$sql = '
+	SELECT mov_id, mov_title, mov_synopsis, mov_poster
+	FROM movies
+';
+
+$stmt = $pdo->query($sql);
+
+if (!$stmt->execute()) {
+	print_r($stmt->errorInfo());
+}
+else {
+	$mvliste = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	//$maxpage = intval(count(?????));
+	//echo "Pages extra: $maxpage";
+	var_dump($mvliste);
+}
+
+
+include_once 'views/header.php';
+include_once 'views/catalogView.php';
+include_once 'views/footer.php';
