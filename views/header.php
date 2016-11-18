cde8f2206b5fef4f5573a75f5873e1ed7fd16885
pm352 <furfari.paul@gmail.com>
2016-11-18 16:01:24 +0100

    update

--- views/header.php
+++ views/header.php
@@ -26,9 +26,9 @@
 				<!-- Collect the nav links, forms, and other content for toggling -->
 				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 					<ul class="nav navbar-nav">
-						<li class="active"><a href="#">Acceuil</a></li>
+						<li class="active"><a href="index.php">Acceuil</a></li>
 						<li><a href="catalog.php">Catégories</a></li>
-						<li><a href="movie.php">Ajout d'un film</a></li>
+						<li><a href="admin/movies.php">Ajout d'un film</a></li>
 					</ul>
 					<form class="navbar-form navbar-right" role="search">
 						<div class="form-group">
--- views/indexView.php
+++ views/indexView.php
@@ -2,8 +2,11 @@
 <div class="row">
 	<div class="col-xs-2"></div>
 	<div class="col-xs-8">
-		<p>La page d’accueil présente brièvement le concept et affiche en premier lieu un grand et gros champ de recherche parmi les films. Une liste non exhaustive de catégories est affichée. Le nombre de films de chaque catégorie (affichée sur l’accueil) est mis entre parenthèses à droite du nom de la catégorie. Les derniers films ajoutés seront aussi affichés avec leur affiche et leur nom.
-		</p>
+		<div class="jumbotron">
+			<h1>Projet MOD</h1>
+			<p>La page d’accueil présente brièvement le concept et affiche en premier lieu un grand et gros champ de recherche parmi les films. Une liste non exhaustive de catégories est affichée. Le nombre de films de chaque catégorie (affichée sur l’accueil) est mis entre parenthèses à droite du nom de la catégorie. Les derniers films ajoutés seront aussi affichés avec leur affiche et leur nom.
+			</p>
+		</div>
 	</div> 
 	<div class="col-xs-2"></div>
 </div>
