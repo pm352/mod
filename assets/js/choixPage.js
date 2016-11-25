5f17e4e8a70b787f6a178b83a9e25182501fd97d
DiPaLu <seiya@windowslive.com>
2016-11-25 09:08:13 +0100

    Tri

--- assets/js/choixPage.js
+++ assets/js/choixPage.js
@@ -1,17 +1,13 @@
-    var page = "";
-    var tri = "";
 $(document).ready(function(){
 	//console.log("Hello");
     
 	$("#choix_page").change(function(){
-        page = window.location.href="catalog.php?page="+$("#choix_page").val();
+        window.location.href="catalog.php?page=" + $("#choix_page").val() + "&tri=" + $("#tri").val();
 	});
-        console.log(page);
     
     $("#tri").change(function(){
-        tri = window.location.href="catalog.php?tri="+$("#tri").val();
+        window.location.href="catalog.php?tri=" +$("#tri").val();
 	});
-        console.log(tri);
     
     
 }); //FIN JQUERY !!!!!
