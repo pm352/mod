d723635e351cbca8105480eaaed2f0de3927cfee
DiPaLu <seiya@windowslive.com>
2016-11-25 01:06:45 +0100

    js

--- assets/js/choixPage.js
+++ assets/js/choixPage.js
@@ -1,15 +1,17 @@
+    var page = "";
+    var tri = "";
 $(document).ready(function(){
 	//console.log("Hello");
     
 	$("#choix_page").change(function(){
-	   var page =	 window.location.href="catalog.php?page="+$("#choix_page").val();
-        console.log(page);
+        page = window.location.href="catalog.php?page="+$("#choix_page").val();
 	});
+        console.log(page);
     
     $("#tri").change(function(){
-	   var tri = window.location.href="catalog.php?tri="+$("#tri").val();
-       console.log(tri);
+        tri = window.location.href="catalog.php?tri="+$("#tri").val();
 	});
+        console.log(tri);
     
     
 }); //FIN JQUERY !!!!!
